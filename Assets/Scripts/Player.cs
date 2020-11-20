7afd2ad8c8b353af4ac247452cf561a4c15e19fe
Kim Kijung <rlwjd1504@gmail.com>
2020-11-20 09:43:18 +0900

    Init Project with Player move function

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchLeft;
+    public bool isTouchRight;
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        if ((h > 0 && isTouchRight) || (h < 0 && isTouchLeft))
+        {
+            h = 0;
+        }
+        if ((v > 0 && isTouchTop) || (v < 0 && isTouchBottom))
+        {
+            v = 0;
+        }
+        anim.SetInteger("Input", (int)h);
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            if (collision.gameObject.tag == "Border")
+            {
+                switch (collision.gameObject.name)
+                {
+                    case "Top":
+                        isTouchTop = true;
+                        break;
+                    case "Bottom":
+                        isTouchBottom = true;
+                        break;
+                    case "Right":
+                        isTouchRight = true;
+                        break;
+                    case "Left":
+                        isTouchLeft = true;
+                        break;
+                }
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch(collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+            }
+        }
+    }
+}
