bc61fb077be0544d120cb1e485a4660f32cd30aa
Kim Kijung <rlwjd1504@gmail.com>
2020-11-20 11:19:40 +0900

    Add Player Bullet Fire

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public int health;
+    public Sprite[] sprites;
+
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * speed;
+    }
+
+    void OnHit(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    bool isTouchTop;
+    bool isTouchBottom;
+    bool isTouchLeft;
+    bool isTouchRight;
+
+    public GameObject bulletObj1;
+    public GameObject bulletObj2;
+
     public float speed;
-    public bool isTouchTop;
-    public bool isTouchBottom;
-    public bool isTouchLeft;
-    public bool isTouchRight;
+    public int fireLevel;
+    public float maxFireDelay;
+    public float curFireDelay;
 
     Animator anim;
 
@@ -24,6 +32,67 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+        Fire();
+        Reload();
+    }
+
+    private void Reload()
+    {
+        curFireDelay += Time.deltaTime;
+    }
+
+    private void Fire()
+    {
+        if (!Input.GetButton("Fire1"))
+        {
+            return;
+        }
+
+        if (curFireDelay < maxFireDelay)
+        {
+            return;
+        }
+
+        switch(fireLevel)
+        {
+            case 1:
+                GameObject bullet = Instantiate(bulletObj1, transform.position, transform.rotation);
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                GameObject bulletR = Instantiate(bulletObj1, transform.position + Vector3.right * 0.1f, transform.rotation);
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                rigidR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+                GameObject bulletL = Instantiate(bulletObj1, transform.position + Vector3.left * 0.1f, transform.rotation);
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                rigidL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletCC = Instantiate(bulletObj2, transform.position, transform.rotation);
+                Rigidbody2D rigidCC = bulletCC.GetComponent<Rigidbody2D>();
+                rigidCC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+                GameObject bulletRR = Instantiate(bulletObj1, transform.position + Vector3.right * 0.35f, transform.rotation);
+                Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                rigidRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+                GameObject bulletLL = Instantiate(bulletObj1, transform.position + Vector3.left * 0.35f, transform.rotation);
+                Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                rigidLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+
+        }
+
+        
+
+        curFireDelay = 0;
+    }
+
+    private void Move()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
@@ -41,7 +110,6 @@ public class Player : MonoBehaviour
         Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
 
         transform.position = curPos + nextPos;
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
